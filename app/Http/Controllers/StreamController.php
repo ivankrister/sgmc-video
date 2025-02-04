6d0666ae2ad6f394e2da9de79e60518c0ce265d3
Ivan Krister Garcia <ivankrister.garcia@gmail.com>
2025-02-04 10:35:58 +0800

    revert

--- app/Http/Controllers/StreamController.php
+++ app/Http/Controllers/StreamController.php
@@ -52,7 +52,8 @@ public function getVideoPlaylist($m3u8Url, $referer, $origin)
             if ($response->successful()) {
                 $body = $response->body();
 
-                Cache::store('octane')->put($cacheKey, $body, 1);
+                // Store the response in cache for 1 second
+                Cache::store('octane')->put($cacheKey, $body, now()->addSeconds(1));
 
                 return response($body, 200)
                     ->header('Content-Type', 'text/plain; charset=utf-8')
