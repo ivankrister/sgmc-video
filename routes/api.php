7423475e1f91ee866326607c7cc319de4142147b
Ivan Krister Garcia <ivankrister.garcia@gmail.com>
2025-02-07 09:39:15 +0800

    fix: route not save

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\StreamController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/video/playlist.m3u8', [StreamController::class, 'index']);
 
 Route::get('/video/{filename}.ts', [StreamController::class, 'show']);
+
+Route::get('/stream/playlist.m3u8', [StreamController::class, 'streamIndex']);
+
+Route::get('/stream/{filename}.ts', [StreamController::class, 'streamShow']);
